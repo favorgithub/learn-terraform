c4bfc5e285629d9ff38a1acf2e7d9b31634e4ed1
favorgithub <gloify@yahoo.com>
2023-06-07 17:41:19 -0500

    Test sample yml

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -17,4 +17,57 @@ output "sample1" {
 
 output "sample-ext" {
   value = "value of sample - ${var.sample}"
+
 }
+
+## Plain Variable
+variable "course" {
+ default = "DevOps Training"
+}
+
+## List Variable
+variable "courses" {
+  default [
+   "DevOps",
+   "AWS",
+   "Python",
+  ]
+
+  }
+
+
+## Map Variables
+variable "course_details" {
+  default = {
+   DevOps = {
+    name = "DevOps"
+    timing = "10am"
+    duration = 90
+   }
+
+   AWS = {
+    name = "AWS"
+    timimg = "11am"
+    duration = 30
+   }
+
+   output "course" {
+     value = var.course
+   }
+
+
+   output "courses" {
+        value = var.courses
+      }
+
+
+   output "course details" {
+     value = var.course_details
+   }
+
+
+
+
+
+
+

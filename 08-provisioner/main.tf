22c53feba395133ab095f5228fd612d9f9a38a4e
favorgithub <gloify@yahoo.com>
2023-06-11 23:21:27 -0500

    Test sample yml

--- 08-provisioner/main.tf
+++ 08-provisioner/main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "web" {
   provisioner "remote-exec" {
       inline = [
         "sudo labauto ansible",
-        "ansible-pull -i localhost, -U https://github.com/devopsblessed/roboshop-ansible main.yml -e env=dev -e role_name=frontend"
+        "ansible-pull -i localhost, -U https://github.com/favorgithub/roboshop-ansible main.yml -e env=dev -e role_name=frontend"
       ]
     }
 }

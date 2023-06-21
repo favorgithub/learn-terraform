2dbd478da64bf2063084b5d4f990e3228663f136
favorgithub <gloify@yahoo.com>
2023-06-20 20:41:25 -0500

    Test sample yml

--- 06-module/main.tf
+++ 06-module/main.tf
@@ -11,3 +11,15 @@
 module "ec2" {
   source = "./ec2"
 }
+
+
+
+output "public_ip_sample1" {
+   value = module.sample1.public_ip
+
+ }
+
+ output "public_ip_sample2" {
+    value = module.sample2.public_ip
+
+  }
--- 07-module-1/main.tf
+++ 07-module-1/main.tf
@@ -14,15 +14,7 @@ module "sample2" {
   name = "sample2"
 }
 
-output "public_ip_sample1" {
-   value = module.sample1.public_ip
 
- }
-
- output "public_ip_sample2" {
-    value = module.sample2.public_ip
-
-  }
 
 
 

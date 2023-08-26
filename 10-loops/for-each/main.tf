06df0eba9cddd8ef05ff011710f5f3119cb8e8bc
favorgithub <gloify@yahoo.com>
2023-08-26 18:10:57 -0500

    Test sample yml

--- 10-loops/for-each/main.tf
+++ 10-loops/for-each/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   for_each = var.instances
   ami           = data.aws_ami.example.id
-  instance_type = "t3.micro"
+  instance_type = lookup(each.value, "instance_type", "t3.small")
 
    tags = {
       Name = each.key
@@ -25,7 +25,7 @@ data "aws_ami" "example" {
 
        frontend = {
          name = "frontend"
-         instance_type = "t3.micro"
+
 }
        catalogue = {
           name = "catalogue"
